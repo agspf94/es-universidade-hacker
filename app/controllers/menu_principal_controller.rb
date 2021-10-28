3e70fe2a992ab1aaf7c39f07e736e2f47f76d0c6
arqueijo <mateus.lino@usp.br>
2021-10-28 00:46:09 -0300

    Atualização de rotas e criação de botões de link

--- app/controllers/menu_principal_controller.rb
+++ app/controllers/menu_principal_controller.rb
@@ -1,4 +1,7 @@
 class MenuPrincipalController < ApplicationController
   def index
   end
+  def jogo
+    render 'jogo/index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :alunos
   root 'welcome#index'
-  resources :menu_principal, only: [:index]
+  resources :menu_principal, only: [:index] do
+    collection do
+      get:jogo
+    end
+  end
+  #resources :jogo, only: [:index]
 end
