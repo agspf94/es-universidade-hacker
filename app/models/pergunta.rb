39bd288fd4399c88f5e6de5bb5c4b56c89b3afba
Daenja <yoonusp9021@usp.br>
2021-11-10 19:25:14 -0300

    oi

--- app/models/pergunta.rb
+++
@@ -1,5 +0,0 @@
-class Pergunta < ApplicationRecord
-  validates :enunciado, presence: { message: "É obrigatório preencher o enunciado!" }
-  validates :alternativas, presence: { message: "É obrigatório preencher pelo menos uma alternativa!" }
-end
-
