24bdcf7bb76cb1814dc5c4a2e27c93ff1f315bff
Anderson Fantin <anderson.fantin@usp.br>
2021-11-10 20:16:21 -0300

    [agspf94] Update migration on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,6 @@ ActiveRecord::Schema.define(version: 2021_11_09_225129) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "usuarios", force: :cascade do |t|
     t.string "email"
     t.string "senha"
